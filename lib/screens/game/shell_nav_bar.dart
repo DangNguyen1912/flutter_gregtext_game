ba74df0a89bd3d77b61bbe5bb38a7d1f3dda4734
Dang Nguyen <huhuyjuhu1@gmail.com>
2026-07-06 11:19:40 -0300

    shell nav bar for the game screens

---
+++ lib/screens/game/shell_nav_bar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class ShellNavBar extends StatefulWidget {
+  final StatefulNavigationShell navigationShell;
+
+  const ShellNavBar({super.key, required this.navigationShell});
+
+  @override
+  State<ShellNavBar> createState() => _ShellNavBarState();
+}
+
+class _ShellNavBarState extends State<ShellNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widget.navigationShell,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: widget.navigationShell.currentIndex,
+        onTap: (index) {
+          widget.navigationShell.goBranch(
+            index,
+            initialLocation: index == widget.navigationShell.currentIndex,
+          );
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.explore), label: 'Explore'),
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Base'),
+          BottomNavigationBarItem(icon: Icon(Icons.build), label: 'Craft'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+        ],
+      ),
+    );
+  }
+}
