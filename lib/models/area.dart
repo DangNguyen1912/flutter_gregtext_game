6d8e5492a8e627c8914e819ac187951127e5b3a3
Dang Nguyen <huhuyjuhu1@gmail.com>
2026-07-06 11:02:06 -0300

    add area model

---
+++ lib/models/area.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Area {
+  String name;
+  Area({required this.name});
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{'name': name};
+  }
+
+  factory Area.fromMap(Map<String, dynamic> map) {
+    return Area(name: map['name'] as String);
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Area.fromJson(String source) =>
+      Area.fromMap(json.decode(source) as Map<String, dynamic>);
+}
