5cf8dd66897c22ff8ee50640e86ac137710597be
Dang Nguyen <huhuyjuhu1@gmail.com>
2026-07-10 13:29:50 -0300

    add Update game user to setting screen (temporary)

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gregtext_game/services/database_service.dart';
+import 'package:provider/provider.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
   @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  Future<void> _updateGameUser() async {
+    final db = Provider.of<DatabaseService>(context, listen: false);
+    var user = await db.getUser();
+    if (user != null) {
+      db.saveUser(user);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Settings"));
+    return Scaffold(
+      appBar: AppBar(title: Text('Settings')),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: _updateGameUser,
+              child: Text('Update game user'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
